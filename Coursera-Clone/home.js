8f68b952ce22cb98aaf7db3febf9b042888f6277
abhaygedam <abhaygedam1999@gmail.com>
2021-07-21 23:13:17 +0530

    login signup function added

--- Coursera-Clone/home.js
+++ Coursera-Clone/home.js
@@ -13,26 +13,77 @@
         
         e.preventDefault();
 
+        document.querySelector(".bg-model").style.display = "none";
+        document.body.style.background = "none";
+
         let userData = document.getElementById("signUPForm");
 
         const name = userData.name.value;
         const email = userData.email.value;
         const password = userData.password.value;
+        let userAllData;
 
-        console.log(name, email, password);
+       if(name!="" && email!="" && password!=""){
+           userAllData = {
+               name,
+               email,
+               password
+           };
+            
+        }else{
+            alert("Fill All Details");
+        }
+        
+        let arr;
+        arr = localStorage.getItem("signUpData");
 
-        document.querySelector(".bg-model").style.display = "none";
-        document.body.style.background = "none";
+        if(arr==null){
+            arr = [];
+        }else{
+            arr = JSON.parse(localStorage.getItem("signUpData"));
+        }
+
+        arr.push(userAllData);
+        localStorage.setItem("signUpData", JSON.stringify(arr));
+        
 
         document.body.style.background = "rgba(0, 0, 0, 0.7)";
         document.querySelector(".bg-model_login").style.display = "flex";
 
         document.querySelector(".close2").addEventListener("click", function () {
-            console.log("**")
             document.querySelector(".bg-model_login").style.display = "none";
             document.body.style.background = "none";
         });
+      
+        }
+   
+        function userLogin(e) {
+            e.preventDefault();
+
+            let form = document.getElementById("login12");
+
+            const username = form.email.value;
+            const password = form.password.value;
+
+            const userAllData = JSON.parse(localStorage.getItem("signUpData"));
+
+            for(let i=0;i<userAllData.length;i++){
+                let u = userAllData[i].email;
+                let p = userAllData[i].password;
+
+                if(u==username && p==password){
+                    console.log("loged in");
+                    document.querySelector(".bg-model_login").style.display = "none";
+                    document.body.style.background = "none";
+                    return;
+                }
+        }
+        alert("Invalid Credentials");
+
+        };
         
-    }
+        
+    
+
 
      
